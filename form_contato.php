a9b13bcf5027788ca93ff348d20c70982e7fe6b5
Ismar Vicente <ismar@uol.com.br>
2020-04-23 16:52:06 -0300

    primeiro arquivo

---
+++ form_contato.php
@@ -0,0 +1,17 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Contato</title>
+	</head>
+	<body>
+		<form method="POST" action="salva_mensagem.php">
+		Nome: <input type="text" name="nome" placeholder="Nome Completo" required></br></br>
+		E-mail: <input type="email" name="email" placeholder="Seu melhor e-mail" required></br></br>
+		Assuntol: <input type="text" name="assunto" placeholder="Assunto do contato" required></br></br>
+	</body>
+</html>
